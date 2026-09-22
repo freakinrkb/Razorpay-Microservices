af044ca684f03ff3fa9b4aff9e6f86e71b1a9ead
freakinrkb <ra7nak@gmail.com>
2026-09-22 22:42:33 +0530

    feat(payment): X-Idempotency-Key replay on initiate + enable Feign/scheduling/auditing on Application classes

--- operations-service/src/main/java/com/razorpay/operations_service/OperationsServiceApplication.java
+++ operations-service/src/main/java/com/razorpay/operations_service/OperationsServiceApplication.java
@@ -2,8 +2,16 @@ package com.razorpay.operations_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditorAwareImpl")
+@EnableSchedulerLock(defaultLockAtMostFor = "10m")
+@EnableFeignClients
+@EnableScheduling
 public class OperationsServiceApplication {
 
 	public static void main(String[] args) {
--- payment-service/src/main/java/com/razorpay/payment_service/PaymentServiceApplication.java
+++ payment-service/src/main/java/com/razorpay/payment_service/PaymentServiceApplication.java
@@ -2,8 +2,16 @@ package com.razorpay.payment_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditorAwareImpl")
+@EnableFeignClients
+@EnableSchedulerLock(defaultLockAtMostFor = "10m")
+@EnableScheduling
 public class PaymentServiceApplication {
 
 	public static void main(String[] args) {
